67fbae83f963be889f15f22cb89e571560369fea
Jake <github@jake.otherinbox.com>
2010-07-26 13:53:18 -0700

    Moved javascript code into template.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -7,22 +7,6 @@ jQuery(function(){
 	jQuery('p.flash').click(function(){$(this).remove();});
 
 /*
-	Being in a separate javascript file, google analytics
-	doesn't 'see' this. Waiting to see if the stats change.
-*/
-	var _gaq = _gaq || [];
-	_gaq.push(['_setAccount', 'UA-17648061-6']);
-	_gaq.push(['_trackPageview']);
-
-	var ga = document.createElement('script'); 
-	ga.type = 'text/javascript'; 
-	ga.async = true;
-	ga.src = ('https:' == document.location.protocol ? 
-		'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-	var s = document.getElementsByTagName('script')[0]; 
-	s.parentNode.insertBefore(ga, s);
-
-/*
 	var root = (location.host == 'ccls.berkeley.edu')?'/buffler':''
 	jQuery.getScript(root + 
 */
