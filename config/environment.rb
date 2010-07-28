47da5d641073c567e876e054841b534151834e9e
Jake <github@jake.otherinbox.com>
2010-07-28 14:44:15 -0700

    No longer extending Rails via preinitializer to define the app_name.  Using RAILS_APP_NAME constant.

--- config/environment.rb
+++ config/environment.rb
@@ -3,14 +3,16 @@
 # Specifies gem version of Rails to use when vendor/rails is not present
 RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 
-ENV['RAILS_ENV'] ||= 'production'
+#ENV['RAILS_ENV'] ||= 'production'
 
 # Bootstrap the Rails environment, frameworks, and default configuration
 require File.join(File.dirname(__FILE__), 'boot')
 
-Rails::Initializer.run do |config|
-	config.app_name = "Buffler"
+#	This constant is used in the ucb_ccls_engine#Document
+#	for controlling the path to documents.
+RAILS_APP_NAME = 'buffler'
 
+Rails::Initializer.run do |config|
 	# Settings in config/environments/* take precedence over those specified here.
 	# Application configuration should go into files in config/initializers
 	# -- all .rb files in that directory are automatically loaded.
