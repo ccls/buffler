f587f32288843653c380dbf869f35b0b058e76ec
Jake <github@jake.otherinbox.com>
2010-07-28 16:45:00 -0700

    Commented out unused code.

--- config/initializers/haml.rb
+++ config/initializers/haml.rb
@@ -1,8 +1,11 @@
-begin
-  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
-rescue LoadError
-  require 'haml' # From gem
-end
-
-# Load Haml and Sass
-Haml.init_rails(binding)
+#	begin
+#	  require File.join(File.dirname(__FILE__), 'lib', 'haml') # From here
+#	rescue LoadError
+#	  require 'haml' # From gem
+#	end
+#	
+#	# Load Haml and Sass
+#	Haml.init_rails(binding)
+#
+#	I don't remember why this is here, so I'm commenting it all out.
+#
