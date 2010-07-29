5c1e7e65e1d44cbb8c7af8ff32a7772067ba925a
Jake <github@jake.otherinbox.com>
2010-07-29 11:46:18 -0700

    Removed a lot of comments and moved aws-s3 to engine.

--- config/environment.rb
+++ config/environment.rb
@@ -9,36 +9,20 @@ RAILS_GEM_VERSION = '2.3.8' unless defined? RAILS_GEM_VERSION
 require File.join(File.dirname(__FILE__), 'boot')
 
 #	This constant is used in the ucb_ccls_engine#Document
+#	and other places like Amazon buckets
 #	for controlling the path to documents.
 RAILS_APP_NAME = 'buffler'
 
 Rails::Initializer.run do |config|
-	# Settings in config/environments/* take precedence over those specified here.
-	# Application configuration should go into files in config/initializers
-	# -- all .rb files in that directory are automatically loaded.
-
-	# Add additional load paths for your own custom dirs
-	# config.load_paths += %W( #{RAILS_ROOT}/extras )
-#	config.load_paths << "#{RAILS_ROOT}/app/sweepers"
-
-	# Specify gems that this application depends on and have them installed with rake gems:install
-	# config.gem "bj"
-	# config.gem "hpricot", :version => '0.6', :source => "http://code.whytheluckystiff.net"
-	# config.gem "sqlite3-ruby", :lib => "sqlite3"
-	# config.gem "aws-s3", :lib => "aws/s3"
-
-#	move me into engine
-	config.gem "aws-s3", :lib => "aws/s3"
-#	may need to add these to warbler
-#Successfully installed xml-simple-1.0.12
-#Successfully installed builder-2.1.2
-#Successfully installed mime-types-1.16
-#Successfully installed aws-s3-0.6.2
-
 
 	# This is set to /buffler in environments/production.rb
+	#	Actually, this is the ROOT app so it should stay blank!
 	config.action_controller.relative_url_root = ''
 	
+
+	#	Move all of this into engine since
+	#	it is the same for all 3 apps.
+
 	if RUBY_PLATFORM =~ /java/
 		#	For functionality with rvm/jruby
 		#	I expected to have to change database.yml for this but didn't
@@ -48,10 +32,6 @@ Rails::Initializer.run do |config|
 		#	Additional jruby specific jars required in the war
 		config.gem 'jdbc-sqlite3', :lib => 'jdbc/sqlite3'
 		config.gem 'jruby-openssl', :lib => 'openssl'
-#
-#	I think this is only for Surveyor and therefore HomeX
-#
-#		config.gem 'haml'
 	else
 
 		# If using mysql ...
@@ -68,34 +48,17 @@ Rails::Initializer.run do |config|
 	end
 
 	config.gem "chronic"   #		http://chronic.rubyforge.org/
-#	config.gem "packet"    #		http://packet.rubyforge.org/
 	config.gem 'will_paginate'
 	config.gem 'fastercsv'
 
 
-	
-
-
-	# Only load the plugins named here, in the order given (default is alphabetical).
-	# :all can be used as a placeholder for all plugins not explicitly named
 	# config.plugins = [ :exception_notification, :ssl_requirement, :all ]
 
-	# Skip frameworks you're not going to use. To use Rails without a database,
-	# you must remove the Active Record framework.
-	# config.frameworks -= [ :active_record, :active_resource, :action_mailer ]
 	config.frameworks -= [ :active_resource ]
 
-	# Activate observers that should always be running
-	# config.active_record.observers = :cacher, :garbage_collector, :forum_observer
-
-	# Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
+	# Set Time.zone default to the specified zone and make Active Record 
+	#	auto-convert to this zone.
 	# Run "rake -D time" for a list of tasks for finding time zone names.
 	config.time_zone = 'UTC'
-
-	# The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
-	# config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}')]
-	# config.i18n.default_locale = :de
-
-#	config.action_view.sanitized_allowed_attributes = 'id', 'class', 'style'
 
 end
