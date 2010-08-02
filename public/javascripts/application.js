7260a1cebb5f6de85086dff7b0c04bcf7a56f78c
Jake <github@jake.otherinbox.com>
2010-08-02 15:50:59 -0700

    Moved some javascript into engine.

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,11 +1,5 @@
 jQuery(function(){
 
-	jQuery('button.link').click(function(){
-		window.location.href = $(this).find('span.href').text();
-	});
-
-	jQuery('p.flash').click(function(){$(this).remove();});
-
 /*
 	var root = (location.host == 'ccls.berkeley.edu')?'/buffler':''
 	jQuery.getScript(root + 
