a2f2ec3eed4bd2d30014deb23d90247a2b36c169
Jake <github@jake.otherinbox.com>
2010-10-20 14:47:05 -0700

    Added 'photos' gem.

--- config/environment.rb
+++ config/environment.rb
@@ -18,6 +18,9 @@ Rails::Initializer.run do |config|
 	config.gem 'jakewendt-ccls_engine',
 		:lib    => 'ccls_engine'
 
+	config.gem 'jakewendt-photos',
+		:lib    => 'photos'
+
 	#	Move all of this into engine since
 	#	it is the same for all 3 apps.
 
