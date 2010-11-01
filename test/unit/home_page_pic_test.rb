d7719baaea3638d3a58c471d745563180bf35d5a
Jake <github@jake.otherinbox.com>
2010-11-01 15:49:18 -0700

    Using assert_should_require_attribute_length

--- test/unit/home_page_pic_test.rb
+++ test/unit/home_page_pic_test.rb
@@ -3,6 +3,7 @@ require File.dirname(__FILE__) + '/../test_helper'
 class HomePagePicTest < ActiveSupport::TestCase
 
 	assert_should_require(:title)
+	assert_should_require_attribute_length(:title,:minimum => 4)
 
 	test "should create home_page_pic" do
 		assert_difference 'HomePagePic.count' do
@@ -11,13 +12,6 @@ class HomePagePicTest < ActiveSupport::TestCase
 				"#{@object.errors.full_messages.to_sentence}"
 		end
 		@object.destroy
-	end
-
-	test "should require 4 char title" do
-		assert_no_difference 'HomePagePic.count' do
-			object = create_object(:title => 'Hey')
-			assert object.errors.on(:title)
-		end
 	end
 
 	test "should return random HPP" do
